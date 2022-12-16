92c4f70aade53767d2a8d9860414165e553e897e
Victor <victordiasdeveloper@gmail.com>
2022-12-16 18:35:18 -0300

    showing tenant and subdomain

--- pages/[tenant].js
+++ pages/[tenant].js
@@ -1,8 +1,27 @@
 import { useRouter } from 'next/router'
 
-export default function Tenant() {
+export default function Tenant(props) {
+  const { tenant, subdomain } = props;
+
   const router = useRouter()
-  const { tenant } = router.query
+  // const { tenant } = router.query
+  console.log('Multi-tenant: ', tenant, ' || ', subdomain);
 
   return <p>Tenant: {tenant}</p>
+}
+
+export async function getServerSideProps(context) {
+  const { req, res } = context;
+  const { host } = req.headers;
+  const subdomain = host.split('.')[0];
+  const tenant = subdomain === 'a' ? 'tenantA' : 'tenantB';
+
+  console.log('Multi-tenant: ', tenant, ' || ', subdomain);
+
+  return {
+    props: {
+      tenant,
+      subdomain
+    },
+  };
 }
